fb1f1b4bb3e63de2108394a52b7a5b334860d754
max <mchappycraft@yandex.ru>
2021-11-13 08:44:07 +0500

    add db examples from previous lecture

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -8,10 +8,47 @@ class IndexController
      */
     public $name = 'index';
 
+    /**
+     * @var ArticleRepository
+     */
+    protected $articleRepository;
+
+    public function __construct(ArticleRepository $articleRepository)
+    {
+        $this->articleRepository = $articleRepository;
+    }
+
     public function indexAction()
     {
         return new Response($this->render('index-template', []));
     }
+
+//    public function indexAction(Request $request)
+//    {
+//        $articles = $this->articleRepository->getAll();
+//        return new Response(
+//            $this->render('articles', [
+//                'articles' => $articles
+//            ])
+//        );
+//    }
+
+//    public function showAction(Request $request)
+//    {
+//        $id = $request->getQueryParameter("id");
+//
+//        $article = is_int($id) ? $this->articleRepository->getById($id) : null;
+//
+//        if ($article === null) {
+//            return new Response('Page not found', '404', 'Not found');
+//        }
+//
+//        return new Response(
+//            $this->render('article', [
+//                'article' => $article
+//            ])
+//        );
+//    }
 
     protected function render($templateName, $vars = [])
     {
